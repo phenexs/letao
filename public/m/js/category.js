4d93100f22ceb79c114faf19e43c2accb6a705b3
huanggengzhong <you@example.com>
2018-12-13 13:48:27 +0800

    分类页面左侧渲染页面完成1.0

--- public/m/js/category.js
+++ public/m/js/category.js
@@ -1,5 +1,21 @@
+// 入口函数
+$(function(){
 // 分类的区域滚动
 mui('.mui-scroll-wrapper').scroll({
     indicators: false, //是否显示滚动条
 	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
 });
+// 左侧区域滚动数据渲染
+$.ajax({
+url:'/category/queryTopCategory',
+success:function(obj){
+// console.log(obj);
+// 将模板添加到页面上
+var html=template('categoryLeftTpl',obj)
+$('.main-left ul').html(html);
+}
+
+})    
+})
+
+
